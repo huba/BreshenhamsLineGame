ac568503efdba0f3270486ec2e9b0cc1b04e287d
Huba Nagy <12huba@gmail.com>
2014-12-12 15:46:29 +1300

    Tidied up. moved some functionalities into a jQuery plugin huba/jquery-slideshow.

--- script/game.js
+++ script/game.js
@@ -33,237 +33,122 @@ $(document).ready(function() {
         }
     });
 
-    // Assign callbacks
-    // INTRO 0 CALLBACKS ============
-    function intro_0_enter() {
+    // SET UP GUIDE SLIDESHOW =============================
+    $guide_bubble = $('#control > #information_area > #speech_bubble')
+    var guide = $guide_bubble.slideshow({});
 
-    }
+    // generic next-previous buttons...
+    $guide_bubble.find('.page > .button_area > #lb').click(guide.prev_page);
+    $guide_bubble.find('.page > .button_area > #rb').click(guide.next_page);
 
-    $('#intro_0 > .button_area > a#right').click(intro_1_enter);
+    // INTRO_0 EVENTS ===========================
+    $guide_bubble.find('#intro_0').each(function() {
+        var intro_0 = this;
+        $(intro_0).on('show', function() {
+            $guide_bubble.addClass('overtake');
+        });
 
-    $('#intro_0 > .button_area > a#left').click(good_luck_enter);
-    // ==============================
+        $(intro_0).find('.button_area > #lb').off('click').click(function() {
+            guide.set_page('#good_luck');
+        });
+    });
+    // ==========================================
 
-    // INTRO 1 CALLBACKS ============
-    function intro_1_enter() {
-        setSpeechBubble('intro_1');
-        initiateGame();
-    }
+    // INTRO_1 EVENTS ===========================
+    $guide_bubble.find('#intro_1').each(function() {
+        var intro_1 = this;
+        $(intro_1).on('show', initiate_game);
+    });
+    // ==========================================
 
-    $('#intro_1 > .button_area > a#right').click(intro_2_enter);
-    // ==============================
+    // INTRO_2 EVENTS ===========================
+    $guide_bubble.find('#intro_2').each(function() {
+        var intro_2 = this;
+        $(intro_2).on('show', function() {
 
-    // INTRO 2 CALLBACKS ============
-    function intro_2_enter() {
-        setSpeechBubble('intro_2')
+        });
+    });
+    // ==========================================
 
-        $('.page#intro_2 > div.formula > #A_equation').html(A_eq_init);
-        $('.page#intro_2 > div.formula > #B_equation').html(B_eq_init);
-        $('.page#intro_2 > div.formula > #P_equation').html(P_eq_init);
+    // INTRO_3 EVENTS ===========================
+    $guide_bubble.find('#intro_3').each(function() {
+        var intro_3 = this;
+    });
+    // ==========================================
 
-        var interval = 500;
+    // ITERATION EVENTS =========================
+    $guide_bubble.find('#iteration').each(function() {
+        var iteration = this;
+        $(iteration).on('show', function() {
+            $(this).find('p').hide();
+            $(this).find('.formula > #A_equation').html('A = ' + A);
+            $(this).find('.formula > #B_equation').html('B = ' + B);
+            $(this).find('.formula > #P_equation').html('P = ' + P);
+        })
 
-        // animation for solving A
-        setTimeout(function() {
-            $('.page#intro_2 > div.formula > #A_equation').animate({opacity: '0'}, function() {
-                $('.page#intro_2 > div.formula > #A_equation').html('A = 2 &times ('+ end_y + ' - ' + start_y +')');
-            });
-            $('.page#intro_2 > div.formula > #A_equation').animate({opacity: '1'}, function() {
-                setTimeout(function() {
-                    $('.page#intro_2 > div.formula > #A_equation').animate({opacity: '0'}, function() {
-                        $('.page#intro_2 > div.formula > #A_equation').html('A = 2 &times '+ (end_y - start_y));
-                    });
-                    $('.page#intro_2 > div.formula > #A_equation').animate({opacity: '1'}, function() {
-                        setTimeout(function() {
-                            A = 2 * (end_y - start_y);
-                            $('.page#intro_2 > div.formula > #A_equation').animate({opacity: '0'}, function() {
-                                $('.page#intro_2 > div.formula > #A_equation').html('A = ' + A);
-                            });
-                            $('.page#intro_2 > div.formula > #A_equation').animate({opacity: '1'}, function() {
-                                // Animation for solving B
-                                setTimeout(function() {
-                                    $('.page#intro_2 > div.formula > #B_equation').animate({opacity: '0'}, function() {
-                                        $('.page#intro_2 > div.formula > #B_equation').html('B = A - 2 &times ('+ end_x + ' - ' + start_x +')');
-                                    });
-                                    $('.page#intro_2 > div.formula > #B_equation').animate({opacity: '1'}, function() {
-                                        setTimeout(function() {
-                                            $('.page#intro_2 > div.formula > #B_equation').animate({opacity: '0'}, function() {
-                                                $('.page#intro_2 > div.formula > #B_equation').html('B = ' + A + ' - 2 &times ' + (end_x - start_x));
-                                            });
-                                            $('.page#intro_2 > div.formula > #B_equation').animate({opacity: '1'}, function() {
-                                                setTimeout(function() {
-                                                    $('.page#intro_2 > div.formula > #B_equation').animate({opacity: '0'}, function() {
-                                                        $('.page#intro_2 > div.formula > #B_equation').html('B = ' + A + ' - ' + (2*(end_x - start_x)));
-                                                    });
-                                                    $('.page#intro_2 > div.formula > #B_equation').animate({opacity: '1'}, function() {
-                                                        setTimeout(function() {
-                                                            B = A - 2 * (end_x - start_x)
-                                                            $('.page#intro_2 > div.formula > #B_equation').animate({opacity: '0'}, function() {
-                                                                $('.page#intro_2 > div.formula > #B_equation').html('B = ' + B);
-                                                            });
-                                                            $('.page#intro_2 > div.formula > #B_equation').animate({opacity: '1'}, function() {
-                                                                setTimeout(function() {
-                                                                    // Animate solving for P
-                                                                    $('.page#intro_2 > div.formula > #P_equation').animate({opacity: '0'}, function() {
-                                                                        $('.page#intro_2 > div.formula > #P_equation').html('P = ' + A + ' - (' + end_x + ' - ' + start_x + ')');
-                                                                    });
-                                                                    $('.page#intro_2 > div.formula > #P_equation').animate({opacity: '1'}, function() {
-                                                                        setTimeout(function() {
-                                                                            $('.page#intro_2 > div.formula > #P_equation').animate({opacity: '0'}, function() {
-                                                                                $('.page#intro_2 > div.formula > #P_equation').html('P = ' + A + ' - ' + (end_x - start_x));
-                                                                            });
-                                                                            $('.page#intro_2 > div.formula > #P_equation').animate({opacity: '1'}, function() {
-                                                                                P = A - (end_x - start_x);
-                                                                                setTimeout(function() {
-                                                                                    $('.page#intro_2 > div.formula > #P_equation').animate({opacity: '0'}, function() {
-                                                                                        $('.page#intro_2 > div.formula > #P_equation').html('P = ' + P);
-                                                                                    });
-                                                                                    $('.page#intro_2 > div.formula > #P_equation').animate({opacity: '1'}, function() {
-                                                                                        // Yo dawg. I herd u liked callbacks so we put callbacks into ur
-                                                                                        // callbacks so u can call back after u called back.
-                                                                                        // TODO: make this a bit less nested.
-                                                                                        $('.page#intro_2 > div.button_area').fadeIn();
-                                                                                    });
-                                                                                }, interval);
-                                                                            });
-                                                                        }, interval);
-                                                                    });
-                                                                }, interval);
-                                                            });
-                                                        }, interval);
-                                                    });
-                                                }, interval);
-                                            });
-                                        }, interval);
-                                    });
-                                }, interval);
-                            });
-                        }, interval);
-                    });
-                }, interval);
-            });
-        }, 2000);
-    }
-
-    $('#intro_2 > .button_area > a#right').click(intro_3_enter);
-    // ==============================
-
-    // INTRO 3 CALLBACKS ============
-    function intro_3_enter() {
-        $('#intro_2 > .button_area').hide();
-        setSpeechBubble('intro_3');
-    }
-
-    $('#intro_3 > .button_area > a#right').click(iteration_enter);
-    // ==============================
-
-    // ITERATION CALLBACKS ==========
-    function iteration_enter() {
-        setSpeechBubble('iteration');
-        $('.page#iteration > p').hide();
-
-        $('.page#iteration > div.formula > #A_equation').html('A = ' + A);
-        $('.page#iteration > div.formula > #B_equation').html('B = ' + B);
-        $('.page#iteration > div.formula > #P_equation').html('P = ' + P);
-    }
-
-    $('#iteration > .button_area > a#right').click(function() {
-        $('.page#iteration > p').hide();
-        if (getCurrent().hasClass('end_point')) {
-            // this is the end...
-            final_note_enter();
-        } else {
-            // more lies ahead.
-            if (P < 0) {
-                P += A;
-                $('.page#iteration > p#smaller').fadeIn();
-                var next = $('.pixel_container.option.inline');
-            } else {
-                P += B;
-                if (P == 0) {
-                    $('.page#iteration > p#equal').fadeIn();
+        $(iteration).find('.button_area > #lb').off().click(function() {
+            while (!getCurrent().hasClass('end_point')) {
+                if (P < 0) {
+                    P += A;
+                    var next = $('.pixel_container.option.inline');
                 } else {
-                    $('.page#iteration > p#greater').fadeIn();
+                    P += B;
+                    var next = $('.pixel_container.option.above');
                 }
-                var next = $('.pixel_container.option.above');
+
+                var n_x = parseInt(next.attr('x')); n_y = parseInt(next.attr('y'));
+                setCurrent(n_x, n_y);
             }
+        });
 
-            $('.page#iteration > div.formula > #P_equation').animate({opacity: '0'}, 1500, function() {
-                $('.page#iteration > div.formula > #P_equation').html('P = ' + P);
-                $('.page#iteration > div.formula > #P_equation').animate({opacity: '1'});
-                $('.page#iteration > p').fadeOut(3000);
-            });
-
-            var n_x = parseInt(next.attr('x')); n_y = parseInt(next.attr('y'));
-            setCurrent(n_x, n_y);
-        }
-
-        if (getCurrent().hasClass('end_point')) {
-            $('#iteration > .button_area > a#left').fadeOut();
-            $('#iteration > .button_area > a#right').animate({'border-top-left-radius': '5px', 'border-bottom-left-radius': '5px'});
-            $('.page#iteration > p').hide();
-        }
-    });
-
-    $('#iteration > .button_area > a#left').click(function() {
-        $('.page#iteration > p').hide();
-        while (!getCurrent().hasClass('end_point')) {
-            if (P < 0) {
-                P += A;
-                var next = $('.pixel_container.option.inline');
+        $(iteration).find('.button_area > #rb').off().click(function() {
+            if (getCurrent().hasClass('end_point')) {
+                // this is the end...
+                $guide_bubble.next_page();
             } else {
-                P += B;
-                var next = $('.pixel_container.option.above');
+                // more lies ahead.
+                if (P < 0) {
+                    P += A;
+                    var next = $('.pixel_container.option.inline');
+                } else {
+                    P += B;
+                    if (P == 0) {
+                    } else {
+                    }
+                    var next = $('.pixel_container.option.above');
+                }
+
+                var n_x = parseInt(next.attr('x')); n_y = parseInt(next.attr('y'));
+                setCurrent(n_x, n_y);
             }
-
-            var n_x = parseInt(next.attr('x')); n_y = parseInt(next.attr('y'));
-            setCurrent(n_x, n_y);
-        }
-
-        setCurrent(end_x, end_y);
-        $('#iteration > .button_area > a#left').fadeOut();
-        $('#iteration > .button_area > a#right').animate({'border-top-left-radius': '5px', 'border-bottom-left-radius': '5px'});
+        });
     });
-    // ==============================
+    // ==========================================
 
-    // FINAL NOTE CALLBACKS =========
-    function final_note_enter() {
-        setSpeechBubble('final_note');
-        $('#iteration > .button_area > a#left').show();
-        $('#iteration > .button_area > a#right').animate({'border-top-left-radius': '0px', 'border-bottom-left-radius': '0px'});
-    }
+    // FINAL_NOTE EVENTS ========================
+    $guide_bubble.find('#final_note').each(function() {
+        var final_note = this;
+        $(final_note).find('.button_area > #lb').off().click(function() {
+            guide.set_page('#intro_1');
+        });
+    });
+    // ==========================================
 
-    $('#final_note > .button_area > a#left').click(intro_1_enter);
+    // GOOD_LUCK EVENTS =========================
+    $guide_bubble.find('#good_luck').each(function() {
+        var good_luck = this;
+        $(good_luck).on('show', function() {
+            $guide_bubble.removeClass('overtake');
+        })
+    });
+    // ==========================================
+    // ====================================================
 
-    $('#final_note > .button_area > a#right').click(good_luck_enter);
-    // ==============================
-
-    // GOOD LUCK CALLBACKS ==========
-    function good_luck_enter() {
-        setSpeechBubble('good_luck');
-    }
-    // ==============================
-
-    // Start the game
-    setSpeechBubble('intro_0');
-    intro_0_enter();
+    // Start guide
+    guide.set_page('#intro_0');
 });
 
-function setSpeechBubble(id) {
-    // Firstly deactivate all speech bubble contents
-    $('#control > #information_area > #speech_bubble > .page.active').removeClass('active');
-
-    // Then activate new content
-    $('#control > #information_area > #speech_bubble > .page#' + id).addClass('active');
-
-    if ($('#control > #information_area > #speech_bubble > .page.active').attr('overtake') == 'true') {
-        $('#control > #information_area > #speech_bubble').addClass('overtake');
-    } else {
-        $('#control > #information_area > #speech_bubble').removeClass('overtake');
-    }
-}
-
-function initiateGame() {
+function initiate_game() {
     /*
     This function generates the starting points.
     */
@@ -273,16 +158,18 @@ function initiateGame() {
     // Then randomize the start point
     start_x = randInt(0, 10);
     start_y = randInt(0, 10);
-    $('#point_a > #x > .val').text(String(start_x));
-    $('#point_a > #y > .val').text(String(start_y));
+    $('.coordinate#x1').html('$$x_{1}=' + start_x + '$$');
+    $('.coordinate#y1').html('$$y_{1}=' + start_y + '$$');
     setCurrent(start_x, start_y);
 
     // And randomize the endpoint based on the startpoint
     end_x = randInt(start_x + 5, 19);
     end_y = randInt(start_y + 1, start_y + (end_x - start_x));
-    $('#point_b > #x > .val').text(String(end_x));
-    $('#point_b > #y > .val').text(String(end_y));
+    $('.coordinate#x2').html('$$x_{2}=' + end_x + '$$');
+    $('.coordinate#y2').html('$$y_{2}=' + end_y + '$$');
     getPixel(end_x, end_y).addClass('end_point').removeClass('off');
+
+    $('.coordinate').parseMath();
 }
 
 function setCurrent(x, y) {
@@ -307,7 +194,6 @@ function getPixel(x, y) {
     // if (y > 19) {y = 19};
     // if (x > 19) {x = 19};
     var selector = '.pixel_container[y=' + String(y) + '][x=' + String(x) + ']';
-    console.log(selector);
     return $(selector);
 }
 
